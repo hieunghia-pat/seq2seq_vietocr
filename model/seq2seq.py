3815f8158b524ee387d6e10bb5bb3e5b1e1be34d
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-02-23 09:34:31 +0700

    update model.seq2seq.get_predictions

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -1,9 +1,11 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
+
+from data_utils.vocab import Vocab
 from model.feature_extractor import FeatureExtractor
 from model.encoder import Encoder
 from model.decoder import Decoder
-from torch.nn import functional as F
 
 class Seq2Seq(nn.Module):
     def __init__(self, extractor, vocab, image_h, enc_hid_dim, dec_hid_dim, emb_dim, dropout=0.1):
@@ -29,20 +31,24 @@ class Seq2Seq(nn.Module):
 
         return F.log_softmax(outputs, dim=-1) # (bs, w, vocab_size)
 
-    def get_predictions(self, images, vocab, max_len):
+    def get_predictions(self, images, vocab: Vocab, max_len: int):
         self.eval()
-        
+
         features = self.feature_extractor(images)
         encoded_features, hidden_states = self.encoder(features)
         bs = images.shape[0]
-        # applying the teacher-forcing mechanisms
         targets = torch.tensor([vocab.sos_idx]*bs, dtype=torch.int, device=images.device).reshape(bs, 1)
+        eos_mark = torch.tensor([0]*bs).to(float).to(images.device)
         for t in range(max_len):
             input = targets[:, -1].unsqueeze(-1)
             output, hidden_states, _ = self.decoder(input, hidden_states, encoded_features)
             # append new predicted word to the sequence
             target = output.argmax(dim=-1) # (bs, 1)
+            target = torch.where(eos_mark == 1, vocab.padding_idx, target)
             targets = torch.cat([targets, target.unsqueeze(1)], dim=-1)
+            eos_mark = torch.where(target.squeeze(-1) == vocab.eos_idx, 1., eos_mark)
+            if eos_mark.mean() == 1:
+                break
 
         self.train()
 
